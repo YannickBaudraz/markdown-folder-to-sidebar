b3c974c26f9e717feeba827da5ffa2366811fd9d
Yannick Clifford <yannickbaudrazdev@gmail.com>
2023-01-13 08:24:28 +0100

    Generate a GitHub sidebar from Md headings

--- src/index.js
+++ src/index.js
@@ -17,42 +17,52 @@ if (!options.folder) {
 
 const md = new MarkdownIt();
 
-let sidebarContent = createSidebar(options.folder, 0);
-sidebarContent = `# Navigation\n\n${sidebarContent}`;
+const sidebarHeader = '# Navigation\n\n';
+const sidebarContent = sidebarHeader + generateSidebarContent(options.folder);
 
 const sidebarPath = path.join(process.cwd(), '_Sidebar.md');
 fs.writeFileSync(sidebarPath, sidebarContent);
+
 console.log(`The sidebar has been written to ${sidebarPath}`);
 
-function createSidebar(folder, level = 0) {
-  let sidebarContent = '';
+function generateSidebarContent(folder, level = 0) {
+  let filesContent = '';
+  let foldersContent = '';
+  const indent = '    '.repeat(level);
 
   const files = fs.readdirSync(folder);
   files.forEach((file) => {
     const filePath = path.join(folder, file);
-    const indentSize = '    ';
-    const indent = indentSize.repeat(level);
 
     if (fs.lstatSync(filePath).isFile()) {
       if (path.extname(file) !== '.md') {
         return;
       }
 
-      const fileContent = fs.readFileSync(filePath, 'utf8');
-      const parsedContent = md.parse(fileContent, {});
-
-      parsedContent.forEach((token, index) => {
-        if (token.type === 'heading_open') {
-          const text = parsedContent[index+1].content;
-          const link = path.basename(file, path.extname(file));
-          sidebarContent += `${indent}- [${text}](${link})\n`;
-        }
-      });
+      const mdList = extractMdH1ToListEl(filePath, indent)
+      filesContent += mdList;
     } else {
-      sidebarContent += `${indent}- ${file}\n`;
-      sidebarContent += createSidebar(filePath, level + 1);
+      foldersContent += `${indent}- ${file}\n`;
+      foldersContent += generateSidebarContent(filePath, level + 1);
     }
   });
 
-  return sidebarContent;
+  return filesContent + foldersContent;
+}
+
+function extractMdH1ToListEl(filePath, indent) {
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const parsedContent = md.parse(fileContent, {});
+
+  let listElement = '';
+  parsedContent.forEach((token, index) => {
+    if (token.type === 'heading_open') {
+      const text = parsedContent[index + 1].content;
+      const file = path.basename(filePath);
+      const link = path.basename(file, path.extname(file));
+      listElement += `${indent}- [${text}](${link})\n`;
+    }
+  });
+
+  return listElement;
 }
