ff81efe75c079daee57cae32b99bb95747c7cf0a
Yannick Clifford <yannickbaudrazdev@gmail.com>
2023-01-13 19:02:13 +0100

    Use glob package to get all files in a folder recursively
    Code refactoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 #! /usr/bin/env node
 const fs = require('fs');
-const MarkdownIt = require('markdown-it');
+const md = require('markdown-it')();
 const path = require('path');
+const glob = require('glob');
 const {program} = require('commander');
 const {version} = require('../package.json');
 
@@ -10,60 +11,99 @@ program.version(version).
         parse(process.argv);
 
 const options = program.opts();
-
-if (!options.folder) {
+if (!program.opts().folder) {
   console.error('Error: Please specify a folder containing markdown files.');
   process.exit(1);
 }
 
-const md = new MarkdownIt();
-
 const sidebarHeader = '# Navigation\n\n';
 const sidebarContent = sidebarHeader + generateSidebarContent(options.folder);
-
-const sidebarPath = path.join(process.cwd(), '_Sidebar.md');
-fs.writeFileSync(sidebarPath, sidebarContent);
-
-console.log(`The sidebar has been written to ${sidebarPath}`);
+writeSidebarInFile(sidebarContent);
 
 function generateSidebarContent(folder, level = 0) {
   let filesContent = '';
   let foldersContent = '';
-  const indent = '    '.repeat(level);
 
   const files = fs.readdirSync(folder);
   files.forEach((file) => {
     const filePath = path.join(folder, file);
 
-    if (fs.lstatSync(filePath).isFile()) {
-      if (path.extname(file) !== '.md') {
-        return;
-      }
-
-      const mdList = extractMdH1ToListEl(filePath, indent);
-      filesContent += mdList;
-    } else {
-      foldersContent += `${indent}- ${file}\n`;
-      foldersContent += generateSidebarContent(filePath, level + 1);
+    const result = extractPathContent(filePath, level);
+    if (!result) {
+      return;
     }
+
+    filesContent += result.filesContent;
+    foldersContent += result.foldersContent;
   });
 
   return filesContent + foldersContent;
 }
 
-function extractMdH1ToListEl(filePath, indent) {
+function extractPathContent(filePath, level) {
+  const extract = {filesContent: '', foldersContent: ''};
+
+  if (fs.lstatSync(filePath).isFile()) {
+    return extractFileContent(filePath, level, extract);
+  }
+
+  return extractFolderContent(filePath, level, extract);
+}
+
+function extractFileContent(filePath, level, extract) {
+  if (path.extname(getFile(filePath)) !== '.md') {
+    return;
+  }
+
+  extract.filesContent += extractMdH1ToListEl(filePath, level);
+  return extract;
+}
+
+function extractMdH1ToListEl(filePath, level) {
   const fileContent = fs.readFileSync(filePath, 'utf8');
   const parsedContent = md.parse(fileContent, {});
 
   let listElement = '';
   parsedContent.forEach((token, index) => {
-    if (token.type === 'heading_open' && token.tag === 'h1') {
-      const text = parsedContent[index + 1].content;
-      const file = path.basename(filePath);
-      const link = path.basename(file, path.extname(file));
-      listElement += `${indent}- [${text}](${link})\n`;
+    const isH1 = (token.type === 'heading_open') && (token.tag === 'h1');
+    if (!isH1) {
+      return;
     }
+
+    const text = parsedContent[index + 1].content;
+    const link = getFile(filePath, true);
+    listElement += `${getIndent(level)}- [${text}](${link})\n`;
   });
 
   return listElement;
+}
+
+function extractFolderContent(filePath, level, extract) {
+  if (!hasMdFilesRecursively(filePath)) {
+    return;
+  }
+
+  extract.foldersContent += `${getIndent(level)}- ${getFile(filePath)}\n`;
+  extract.foldersContent += generateSidebarContent(filePath, level + 1);
+  return extract;
+}
+
+function hasMdFilesRecursively(directory) {
+  const files = glob.sync('**/*', {cwd: directory});
+  return files.some((file) => path.extname(file) === '.md');
+}
+
+function getIndent(level) {
+  return '    '.repeat(level);
+}
+
+function getFile(filePath, withoutExtension = false) {
+  const ext = withoutExtension ? path.extname(filePath) : '';
+  return path.basename(filePath, ext);
+}
+
+function writeSidebarInFile(content) {
+  const sidebarPath = path.join(process.cwd(), '_Sidebar.md');
+  fs.writeFileSync(sidebarPath, content);
+  console.log(`The sidebar has been written to ${sidebarPath}`);
 }
